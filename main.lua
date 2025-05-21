0e9abec46ca47bee3bc05490a2eab9940a7c9003
Gigot <marioluca.nicoletti@free.fr>
2025-05-21 12:43:43 +0200

    warning in ingsoc fixed
    added checking on "has it triggered" before returning anything

--- main.lua
+++ main.lua
@@ -632,7 +632,7 @@ SMODS.Joker {
         end
         
         if context.repetition and context.cardarea == G.hand then
-            if (next(context.card_effects[1]) or #context.card_effects > 1) then
+            if (next(context.card_effects[1]) or #context.card_effects > 1) and card.ability.extra.ef > 0 then
                 return {
                     message = localize('k_again_ex'),
                     repetitions = card.ability.extra.ef,
@@ -641,10 +641,9 @@ SMODS.Joker {
             end
         end
 
-        if context.hand_drawn then
+        if context.hand_drawn or context.end_of_round then
             card.ability.extra.ef = 0
         end
-        
     end
 }
 
